4539ce5485dae836780c31919129611bf0154550
NEPanchuk <44090740+NEPanchuk@users.noreply.github.com>
2019-10-08 21:18:16 +0500

    Commit

---
+++ TestTask/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TestTask.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateBirth { get; set; }
+        public bool Gender { get; set; }
+        public int? ParentId { get; set; }
+
+        public ICollection<Relative> Relatives { get; set; }
+    }
+}
---
+++ TestTask/Models/User_parent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestTask.Models
+{
+    public class User_parent
+    {
+        public string Name { get; set; }
+        public DateTime DateBirth { get; set; }
+        public bool Gender { get; set; }
+        public string NameParent { get; set; }
+      
+    }
+}
